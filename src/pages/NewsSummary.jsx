aa84960968c3b4ef6c54f345f78d1da145d6bfa2
Ramkalyan7 <yramkalyan0707@gmail.com>
2023-09-27 23:56:28 +0530

    added res navbar

--- src/pages/NewsSummary.jsx
+++ src/pages/NewsSummary.jsx
@@ -7,117 +7,121 @@ import db from "../../firebase";
 // Dummy data for news articles with images
 
 function NewsSummary() {
-    const [filter, setFilter] = useState("All"); // Default filter
-    const [sortOrder, setSortOrder] = useState("asc"); // Default sorting order
-    const [news, setNews] = useState([]);
-    const navigate = useNavigate();
+  const [filter, setFilter] = useState("All"); // Default filter
+  const [sortOrder, setSortOrder] = useState("asc"); // Default sorting order
+  const [fetchedNews, setFetchedNews] = useState([]);
+  const [news, setNews] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const jwtToken = getCookie("jwtToken");
-        if (!jwtToken) {
-            // Redirect to the desired page
-            navigate("/");
-        }
-    }, []);
+  useEffect(() => {
+    const jwtToken = getCookie("jwtToken");
+    if (!jwtToken) {
+      // Redirect to the desired page
+      navigate("/");
+    }
+  }, [navigate]);
 
-    // Filter and sort the news articles based on the selected sentiment and sorting order
-    useEffect(() => {
-        let filteredNews =
-            filter === "All"
-                ? news
-                : news.filter((news) => news.sentiment === filter);
-        // Sort the filtered news based on the selected sorting order
-        filteredNews = filteredNews.sort((a, b) => {
-            const dateA = new Date(a.date);
-            const dateB = new Date(b.date);
-            return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
-        });
-        setNews(filteredNews);
-    }, [filter, news, sortOrder]);
+  // Filter and sort the news articles based on the selected sentiment and sorting order
+  useEffect(() => {
+    let filteredNews =
+      filter === "All"
+        ? fetchedNews
+        : fetchedNews.filter((news) => news.sentiment === filter);
+    // Sort the filtered news based on the selected sorting order
+    filteredNews = filteredNews.sort((a, b) => {
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
+    });
+    setNews(filteredNews);
+  }, [filter, fetchedNews, sortOrder]);
 
-    useEffect(() => {
-        // read data from firebase firestore
-        const fetchNews = async () => {
-            // fetch all documents from news collection in firebase
-            db.collection("news").onSnapshot((snapshot) => {
-                console.log(snapshot.docs.map((doc) => doc.data()));
-                setNews(snapshot.docs.map((doc) => doc.data()));
-            });
-        };
+  useEffect(() => {
+    // read data from firebase firestore
+    const fetchNews = async () => {
+      // fetch all documents from news collection in firebase
+      db.collection("news").onSnapshot((snapshot) => {
+        console.log(snapshot.docs.map((doc) => doc.data()));
+        setFetchedNews(snapshot.docs.map((doc) => doc.data()));
+      });
+    };
 
-        fetchNews();
-    }, []);
+    fetchNews();
+  }, []);
 
-    return (
-        <div className="bg-gray-100 min-h-screen p-4">
-            <div className="max-w-5xl mx-auto">
-                {/* Filter buttons */}
-                <div className="flex justify-center space-x-4 mb-8">
-                    <button
-                        className={`px-8 py-2 border rounded-full ${
-                            filter === "All"
-                                ? "bg-blue-500 text-white"
-                                : "bg-white hover:bg-blue-200"
-                        }`}
-                        onClick={() => setFilter("All")}
-                    >
-                        All
-                    </button>
-                    <button
-                        className={`px-8 py-2 border rounded-full ${
-                            filter === "Positive"
-                                ? "bg-green-500 text-white"
-                                : "bg-white hover:bg-green-200"
-                        }`}
-                        onClick={() => setFilter("Positive")}
-                    >
-                        Positive
-                    </button>
-                    <button
-                        className={`px-8 py-2 border rounded-full ${
-                            filter === "Neutral"
-                                ? "bg-gray-500 text-white"
-                                : "bg-white hover:bg-gray-200"
-                        }`}
-                        onClick={() => setFilter("Neutral")}
-                    >
-                        Neutral
-                    </button>
-                    <button
-                        className={`px-8 py-2 border rounded-full ${
-                            filter === "Negative"
-                                ? "bg-red-500 text-white"
-                                : "bg-white hover:bg-red-200"
-                        }`}
-                        onClick={() => setFilter("Negative")}
-                    >
-                        Negative
-                    </button>
+  return (
+    <div className="bg-gray-100 min-h-screen p-4">
+      <div className="max-w-5xl mx-auto">
+        {/* Filter buttons */}
+        <div className="flex flex-wrap justify-center space-x-4 space-y-4 md:space-y-0 mb-8">
+          <button></button>
 
-                    {/* Sorting dropdown */}
-                    <select
-                        className="px-8 py-2 border rounded-full bg-white hover:bg-gray-200 outline-none"
-                        value={sortOrder}
-                        onChange={(e) => setSortOrder(e.target.value)}
-                    >
-                        <option value="asc">Sort by Date (Asc)</option>
-                        <option value="desc">Sort by Date (Desc)</option>
-                    </select>
-                </div>
-                <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
-                    {news.map((news, i) => (
-                        <Link
-                            to={`/article`} // Specify the article route (replace with your route structure)
-                            key={i}
-                            className="hover:no-underline"
-                        >
-                            <NewsItem news={news} />
-                        </Link>
-                    ))}
-                </div>
-            </div>
+          <button
+            className={`sm:px-8   sm:py-2 px-4 text-sm sm:text-base py-1 border rounded-full ${
+              filter === "All"
+                ? "bg-indigo-500 text-white"
+                : "bg-white hover:bg-green-200"
+            } `}
+            onClick={() => setFilter("All")}
+          >
+            All
+          </button>
+
+          <button
+            className={`sm:px-8 sm:py-2 px-4 text-sm sm:text-base py-1 border rounded-full ${
+              filter === "Positive"
+                ? "bg-green-500 text-white"
+                : "bg-white hover:bg-green-200"
+            }`}
+            onClick={() => setFilter("Positive")}
+          >
+            Positive
+          </button>
+          <button
+            className={`sm:px-8 sm:py-2 px-4 text-sm sm:text-base py-1 border rounded-full ${
+              filter === "Neutral"
+                ? "bg-gray-500 text-white"
+                : "bg-white hover:bg-gray-200"
+            }`}
+            onClick={() => setFilter("Neutral")}
+          >
+            Neutral
+          </button>
+          <button
+            className={`sm:px-8 sm:py-2 px-4 text-sm sm:text-base py-1 border rounded-full ${
+              filter === "Negative"
+                ? "bg-red-500 text-white"
+                : "bg-white hover:bg-red-200"
+            }`}
+            onClick={() => setFilter("Negative")}
+          >
+            Negative
+          </button>
+
+          {/* Sorting dropdown */}
+          <select
+            className="px-8 py-2 border rounded-full bg-white hover:bg-gray-200 outline-none             sm:px-8 sm:py-2 text-sm sm:text-base "
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="asc">Sort by Date (Asc)</option>
+            <option value="desc">Sort by Date (Desc)</option>
+          </select>
         </div>
-    );
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+          {news.map((news, i) => (
+            <Link
+              to={`/article`} // Specify the article route (replace with your route structure)
+              key={i}
+              className="hover:no-underline"
+            >
+              <NewsItem news={news} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewsSummary;
