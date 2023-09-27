f302f482cc601d381b465bba980df334233214e2
Rohit <rk212300@outlook.com>
2023-09-27 17:04:19 +0530

    profile and data added

--- src/pages/NewsItem.jsx
+++ src/pages/NewsItem.jsx
@@ -1,18 +1,31 @@
 /* eslint-disable react/prop-types */
 export default function NewsItem({ news }) {
+    const getBorder = () => {
+        if (news.sentiment == "Negative") {
+            return " bg-red-50 ";
+        }
+        if (news.sentiment == "Positive") {
+            return " bg-green-50";
+        }
+        if (news.sentiment == "Neutral") {
+            return " bg-blue-50";
+        }
+    };
+    // bg-gradient-to-r from-green-300 via-blue-500 to-purple-600
     return (
-        <div className="relative overflow-hidden rounded-lg border border-gray-200 p-4 sm:p-6 lg:p-8 h-full flex-col shadow-xl">
-            <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
+        <div
+            className={`relative overflow-hidden rounded-lg border border-gray-200 p-4 sm:p-6 lg:p-8 h-full flex-col shadow-xl ${getBorder()} hover:scale-105 transition-all duration-300`}
+        >
+            <span className="absolute inset-x-0 bottom-0 h-2 "></span>
 
             <div className="sm:flex sm:justify-between sm:gap-4">
                 <div>
                     <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
-                        {news.title}
+                        {news.Headline}
                     </h3>
 
                     <p className="mt-1 text-xs font-medium text-gray-600">
-                        Unknown
-                        <span className="font-bold"> ({news.source})</span>
+                        <span className="font-bold">{news.Timestamp}</span>
                     </p>
                 </div>
 
@@ -30,13 +43,13 @@ export default function NewsItem({ news }) {
             </div>
 
             <div className="mt-4">
-                <p className="max-w-[40ch] text-sm text-gray-500">
-                    {news.summary}
+                <p className="text-sm text-gray-500">
+                    {news.Description.split(" ").slice(0, 30).join(" ")}{" "}
                     <a
-                        href={"#"}
+                        href={news.Link}
                         target="_blank"
                         rel="noreferrer"
-                        className="text-blue-500 hover:text-blue-700 font-medium"
+                        className="text-blue-500 hover:text-blue-700"
                     >
                         Continue reading...
                     </a>
@@ -46,16 +59,19 @@ export default function NewsItem({ news }) {
             <dl className="flex gap-4 sm:gap-6 mt-5">
                 <div className="flex flex-col-reverse">
                     <dt className="text-sm font-medium text-gray-600">
-                        Publishing date
+                        {news.sentiment.charAt(0).toUpperCase() +
+                            news.sentiment.slice(1)}
                     </dt>
-                    <dd className="text-xs text-gray-500">{news.date}</dd>
+                    <dd className="text-sm text-gray-500">Sentiment</dd>
                 </div>
 
                 <div className="flex flex-col-reverse">
                     <dt className="text-sm font-medium text-gray-600">
-                        Reading time
+                        {Math.round(news.Description.split(" ").length / 20) +
+                            1}{" "}
+                        m
                     </dt>
-                    <dd className="text-xs text-gray-500">1m</dd>
+                    <dd className="text-xs text-gray-500">Reading time</dd>
                 </div>
             </dl>
 
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,67 +1,84 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import axiosInstance from "../api/axiosInstance";
 import { getCookie } from "../Utils/cookieUtils";
 import { useNavigate } from "react-router-dom";
 
 function UserProfile() {
-  const [userData, setUserData] = useState(null);
-  const navigate = useNavigate();
+    const [userData, setUserData] = useState(null);
+    const navigate = useNavigate();
+    console.log("userData", userData);
 
-  useEffect(() => {
-    const jwtToken = getCookie("jwtToken");
-    if (!jwtToken) {
-      // Redirect to the desired page
-      navigate("/");
+    useEffect(() => {
+        const jwtToken = getCookie("jwtToken");
+        if (!jwtToken) {
+            // Redirect to the desired page
+            navigate("/");
+        }
+    }, []);
+
+    useEffect(() => {
+        // Make an API request to fetch user profile data
+        axiosInstance.get("/users/profile").then((response) => {
+            setUserData(response.data);
+        });
+    }, []);
+
+    if (!userData) {
+        return <div>Loading...</div>;
     }
-  }, []);
 
-  useEffect(() => {
-    // Make an API request to fetch user profile data
-    axiosInstance.get("/users/profile").then((response) => {
-      setUserData(response.data);
-    });
-  }, []);
+    return (
+        <div className="bg-gray-100 p-4 min-h-screen flex items-center justify-center">
+            <div className="min-w-[32rem] bg-white p-8 rounded-xl shadow-lg">
+                <div className="text-center">
+                    {/* User image */}
+                    <div className="mb-4">
+                        <img
+                            src={
+                                userData.profileImage ||
+                                `https://api.dicebear.com/7.x/initials/svg?seed=${userData.fullName}}`
+                            }
+                            alt="User"
+                            className="w-24 h-24 mx-auto rounded-full object-cover"
+                        />
+                    </div>
+                    {/* User full name */}
+                    <h2 className="text-3xl font-semibold">
+                        {userData.fullName}
+                    </h2>
+                </div>
 
-  if (!userData) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="bg-gray-100 p-4">
-      <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
-        <div className="text-center">
-          {/* User image */}
-          <div className="mb-4">
-            <img
-              src={userData.profileImage || "/default-user-image.png"}
-              alt="User"
-              className="w-24 h-24 mx-auto rounded-full object-cover"
-            />
-          </div>
-          {/* User full name */}
-          <h2 className="text-2xl font-semibold">{userData.fullName}</h2>
+                {/* User details using material 3 theme */}
+                <div className="mt-8">
+                    <div className="border-t border-gray-200 pt-8 gap-4 flex flex-col">
+                        {/* User email */}
+                        {DetailRow("Email", userData.email)}
+                        {/* User contact number */}
+                        {DetailRow("Contact Number", userData.contactNumber)}
+                        {/* User department */}
+                        {DetailRow("Department", userData.department)}
+                        {/* User region */}
+                        {DetailRow("Region", userData.region)}
+                    </div>
+                </div>
+            </div>
         </div>
-
-        {/* User details */}
-        <div className="mt-4">
-          <div className="mb-2">
-            <span className="font-semibold">Email:</span> {userData.email}
-          </div>
-          <div className="mb-2">
-            <span className="font-semibold">Contact Number:</span>{" "}
-            {userData.contactNumber}
-          </div>
-          <div className="mb-2">
-            <span className="font-semibold">Department:</span>{" "}
-            {userData.department}
-          </div>
-          <div className="mb-2">
-            <span className="font-semibold">Region:</span> {userData.region}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
+const DetailRow = (label, value) => {
+    return (
+        <div className="flex justify-between gap-8">
+            <div className="flex justify-between flex-col gap-2">
+                <div className="text-xl text-black font-semibold">{label}</div>
+                <div className="text-sm font-medium text-gray-600">{value}</div>
+            </div>
+            {/* edit button */}
+            <button className="text-sm text-blue-500 hover:text-blue-700 mb-auto">
+                Edit
+            </button>
+        </div>
+    );
+};
 
 export default UserProfile;
